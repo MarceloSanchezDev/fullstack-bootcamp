b514cf64850f2656fe5fdfdecb191ec5f226fbcb
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2024-02-08 15:14:24 -0300

    realizacion del ejercicio 1.10

--- ejercicios 1/ejercicio 1.6-1.14/src/Statistics.jsx
+++ ejercicios 1/ejercicio 1.6-1.14/src/Statistics.jsx
@@ -1,24 +1,55 @@
-import StatisticLine from './StatisticLine'
+import StatisticLine from "./StatisticLine";
 
-const Statistics = ({good,neutral,bad,total,average,positive}) => {
-    
-    return (
-        <>
-        <h1>statistics</h1>
-        {total === 0 ? 
+const Statistics = ({ good, neutral, bad, total, average, positive }) => {
+  return (
+    <>
+      <h1>statistics</h1>
+      {total === 0 ? (
         <h2>there are no votes</h2>
-        : 
+      ) : (
         <div>
-            
-            <StatisticLine text="good" value={good} />
-            <StatisticLine text="neutral" value={neutral} />
-            <StatisticLine text="bad" value={bad} />
-            <StatisticLine text="total" value={total} />  
-            <StatisticLine text="average" value={average} />
-            <StatisticLine text="positive" value={positive} />
-        </div>}
-        </>
-    )
-}
+          <table>
+            <tbody>
+              <tr>
+                <td>
+                  <StatisticLine text="good" value={good} />
+                </td>
+              </tr>
 
-export default Statistics
+              <tr>
+                <td>
+                  <StatisticLine text="neutral" value={neutral} />
+                </td>
+              </tr>
+
+              <tr>
+                <td>
+                  <StatisticLine text="bad" value={bad} />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <StatisticLine text="total" value={total} />{" "}
+                </td>
+              </tr>
+
+              <tr>
+                <td>
+                  <StatisticLine text="average" value={average} />
+                </td>
+              </tr>
+
+              <tr>
+                <td>
+                  <StatisticLine text="positive" value={positive} />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Statistics;
