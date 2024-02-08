b526abcce4cc9dc70ecfccdaa1458816d8601640
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2024-02-08 10:33:31 -0300

    Agrege la parte1A

---
+++ parte1/parte1A/src/Footer.jsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+    return (
+      <div>
+        greeting app created by <a href='https://github.com/mluukkai'>mluukkai</a>
+      </div>
+    )
+  }
+  export default Footer
---
+++ parte1/parte1A/src/Hello.jsx
@@ -0,0 +1,10 @@
+const Hello = (props) => {
+    return (
+      <div>
+  
+        <p>Hello {props.name}</p>
+      </div>
+    )
+  }
+
+  export default Hello
