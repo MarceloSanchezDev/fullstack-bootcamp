922bbc2755a5c2547a314b45ea9038b5a8cbde45
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2024-02-09 10:35:40 -0300

    realize el ejercicio 2.3

--- ejercicios 2/ejercicio 2.1-2.5/src/components/Content.jsx
+++ ejercicios 2/ejercicio 2.1-2.5/src/components/Content.jsx
@@ -1,14 +1,13 @@
 import Part from "./Part"
 
 const Content = ({course}) => {
-    console.log(course)
     const total = course.reduce(
         (total , exercises) => total + exercises.exercises, 0)
 
     return(
         <div>
             {course.map(inf => <Part key={inf.id} inf={inf} />)}
-            <p>total of {total} exercises</p>
+            <p><b>total of </b>{total} <b>exercises</b></p>
         </div>
     )
 }
