880ffcc5c2610b4de74e1db93bd7688cc4775448
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2024-02-13 19:44:55 -0300

    realize el ejercicio 2.19

--- ejercicios 2/ejercicio 2.18-2.20/src/components/Country.jsx
+++ ejercicios 2/ejercicio 2.18-2.20/src/components/Country.jsx
@@ -1,18 +1,33 @@
-import Languages from "./Languages"
+import { useState } from "react";
+import Languages from "./Languages";
 
-const Country = ({name, capital, area, languages, flags}) => {
+const Country = ({ name, capital, area, languages, flags }) => {
+  const [show, setShow] = useState(false);
 
-    return(
-        <>
-        <h1>{name.common}</h1>
-        <p>{capital}</p>
-        <p>area: {area}</p>
-        <h3>languages:</h3>
-        <ul>
+  const handleShow = () => {
+    setShow(!show);
+  };
+  return (
+    <>
+      {show ? (
+        <div>
+          <h1>{name.common}</h1>
+          <button onClick={handleShow}>show</button>
+        </div>
+      ) : (
+        <div>
+          <h1>{name.common}</h1>
+          <button onClick={handleShow}>unshow</button>
+          <p>{capital}</p>
+          <p>area: {area}</p>
+          <h3>languages:</h3>
+          <ul>
             <Languages languages={languages} />
-        </ul>
-        <img src={flags.png} alt={name} />
-        </>
-    )
-}
-export default Country
+          </ul>
+          <img src={flags.png} alt={name} />
+        </div>
+      )}
+    </>
+  );
+};
+export default Country;
