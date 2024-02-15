a4205ac4474ef228ca31ecc5b43e75262f7289ba
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2024-02-15 11:22:23 -0300

    realize el ejercicio 3.8

--- parte3/parte3A/index.js
+++ parte3/parte3A/index.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 
 app.use(express.json())
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+  
+app.use(unknownEndpoint)
 
 let notes = [
   {
